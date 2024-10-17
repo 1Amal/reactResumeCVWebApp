f33adda671a602dd146dfdb76651c71ac2acf433
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-10-18 06:47:30 +1100

    Added conditional rendering to ResumeDynamicView

--- src/components/ResumeDynamicView.jsx
+++ src/components/ResumeDynamicView.jsx
@@ -9,38 +9,45 @@ export default function ResumeDynamicView({ displayData, displayData2 }) {
 
   const [displayData1, setDisplayData] = useState("test");
 
+  if (displayData === undefined) {
+    return <h1>No Display Data</h1>;
+  } else {
+    return (
+      <>
+        <button
+          onClick={() => {
+            displayData2();
+          }}
+        >
+          Update Display
+        </button>
+
+        <h1>Resume Dynamic View</h1>
+        <div>
+          {/* <h1>Contact Info</h1> */}
+          <h1>{displayData.fullName}</h1>
+
+          <h2>
+            @: {displayData.eMail} Phone: {displayData.mobile}
+          </h2>
+
+          <p></p>
+        </div>
+
+        <div>
+          <h1>Professional Summary</h1>
+        </div>
+
+        <div>
+          <h1>Education and Qualifications</h1>
+        </div>
+
+        <div>
+          <h1>Professional Experience</h1>
+        </div>
+      </>
+    );
+  }
+
   // {console.log({onNavigate})}
-  return (
-    <>
-      <button
-        onClick={() => {
-          displayData2();
-        }}
-      >
-        Update Display
-      </button>
-      <h1>Resume Dynamic View</h1>
-      <div>
-        <h1>Contact Info</h1>
-        {/* <h2>{displayData.fullName}</h2> */}
-
-        {/* <h2>{displayData.eMail}</h2> */}
-        {/* <p>
-        @: {formData.eMail} Phone: {formData.phoneNo}
-      </p> */}
-      </div>
-
-      <div>
-        <h1>Professional Summary</h1>
-      </div>
-
-      <div>
-        <h1>Education and Qualifications</h1>
-      </div>
-
-      <div>
-        <h1>Professional Experience</h1>
-      </div>
-    </>
-  );
 }
