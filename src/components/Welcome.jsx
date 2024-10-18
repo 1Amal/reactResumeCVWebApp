dfe478c85025853107dc73415ad5b627f3841cb8
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-10-18 16:15:34 +1100

    Added introduction text to Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,9 +3,62 @@
 export default function Welcome({ onNavigate }) {
   return (
     <div>
-      <h1>Welcome</h1>
-      <p>This App will let you create a CV/résumé</p>
-      <button onClick={() => onNavigate("EditResume")}>Create Resume</button>
+      <div>
+        <h1>Welcome to CV/résumé Application</h1>
+        <p>
+          This user-friendly tool allows you to easily input your information
+          and generate a professional résumé in just a few clicks.
+        </p>
+
+        <p>
+          Whether you're entering the job market or looking to update your
+          existing CV, this application is designed to help you create a
+          polished document that highlights your skills and experiences.
+        </p>
+        <p>
+          This application is built using React, ensuring a smooth and
+          responsive user experience. With a straightforward interface, you can
+          focus on what matters most—showcasing your qualifications!
+        </p>
+
+        <button onClick={() => onNavigate("EditResume")}>Create Resume</button>
+      </div>
+      <div>
+        <h1>Project Info</h1>
+        <h2>Live Demo</h2>
+        <p>You can access this App from three different hosting providers</p>
+        <p>
+          <a href="https://amal-react-resume.netlify.app">
+            CV/résumé Application hosted on Netlify
+          </a>
+        </p>
+        <p>
+          {" "}
+          <a href="https://amal-react-resume.vercel.app">
+            CV/résumé Application hosted on Vercel
+          </a>{" "}
+        </p>
+
+        <p>
+          {" "}
+          <a href="https://amal-react-resume-webapp.pages.dev">
+            CV/résumé Application hosted on Cloudflare
+          </a>{" "}
+        </p>
+
+        <h2>Source code and Project Info</h2>
+        <p>
+          <a href="https://github.com/1Amal/reactResumeCVWebApp">
+            Source Code hosted on GitHub
+          </a>
+        </p>
+      </div>
+      <div>
+        {" "}
+        <p>
+          <a href="https://1amal.github.io/">Developed by Amal K</a>
+        </p>
+      </div>
     </div>
   );
 }
